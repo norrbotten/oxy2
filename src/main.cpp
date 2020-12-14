69bbbbfb3835527a810f32ad8f77ec202c4a7274
norrbotten <axelsp@gmail.com>
2020-12-14 20:32:10 +0100

    render three spheres

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,17 @@ int main() {
 
   Oxy::Renderer renderer;
 
-  auto error = renderer.load_file("scenes/two_spheres.sdl");
+  auto error = renderer.load_file("scenes/cornell.sdl");
 
   if (error.has_value()) {
     std::cout << error.value() << "\n";
     return 1;
   }
 
-  for (int i = 0; i < renderer.max_samples(); i++)
+  for (int i = 0; i < renderer.max_samples(); i++) {
+    std::cout << i << "\n";
     renderer.sample_once();
+  }
 
-  renderer.save_png("images/two_spheres.png");
+  renderer.save_png("images/cornell.png");
 }
